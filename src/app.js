9ebcad9d83864cbef0860333a3b8dac1a505100a
surajgupta-code <surajguptanew3@gmail.com>
2025-03-19 15:17:30 +0530

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,12 @@
+//https://github.com/surajgupta-code/Tech.Tinder.git
+
+const express = require('express');
+const app = express();
+
+app.use ((req, res) => {
+    res.send('Hello World!');
+})
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
